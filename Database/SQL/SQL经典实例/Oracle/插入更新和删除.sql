48cb86b6592f192ec808a6193f07eb8e6d53513c
ishare <qisheng.ishare@outlook.com>
2020-04-28 00:13:26 +0800

    SQL

--- Database/SQL/SQL经典实例/Oracle/插入更新和删除.sql
+++ Database/SQL/SQL经典实例/Oracle/插入更新和删除.sql
@@ -25,7 +25,7 @@ create table DEPT_EAST
   deptno NUMBER,
   dname  VARCHAR2(32),
   loc    VARCHAR2(32)
-)
+);
 
 insert into dept_east
   (deptno, dname, loc)
@@ -44,19 +44,27 @@ select * from dept_west;
 
 -- 6、多表插入
 -- 把 DEPT 表的数据分别插入到 DEPT_EAST 表、 DEPT_WEST 表和 DEPT_MID 表
+-- INSERT FIRST 和 INSERT ALL 的区别 （后者有可能把同一行数据插入到多个表）
 insert all when loc in
-  ('NEW YORK', 'BOSTON') then into dept_east
-  (deptno, dname, loc)
-values
-  (deptno, dname, loc) when loc = 'CHICAGO' then into dept_mid
-  (deptno, dname, loc)
-values
-  (deptno, dname, loc) else into dept_west
-  (deptno, dname, loc)
-values
-  (deptno, dname, loc)
-  select deptno, dname, loc from dept;
-  
-  
- 
+                ('NEW YORK', 'BOSTON') then into dept_east (deptno, dname, loc)
+values (deptno, dname, loc) when loc = 'CHICAGO' then into dept_mid (deptno, dname, loc)
+values (deptno, dname, loc) else into dept_west (deptno, dname, loc)
+values (deptno, dname, loc)
+select deptno, dname, loc
+from dept;
+
+
+-- 7、禁止插入特定列
+-- 为那些可以向上面三个列中写入数据的用户和程序赋予视图的插入权限。不要把 EMP 表的插入权限授权给用户。
+-- 这样用户插入数据到 NEW_EMPS 视图后就可以创建新的 EMP 记录，但是他们不能为视图定义里不存在的列提供插入值。
+create view new_emps as
+  select empno, ename, job
+  from emp;
+
+-- 向一个简单视图插入数据，数据库服务器会把它转换为针对基础表的插入操作
+insert into new_emps (empno, ename, job)
+values (1, 'Jonathan', 'Editor');
+
+
+-- 8、
 
