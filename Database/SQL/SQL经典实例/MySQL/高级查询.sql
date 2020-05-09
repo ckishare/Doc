ca403cd8d1f16ab7324ca0cd5ec24435e91fa7da
work <qisheng.ishare@outlook.com>
2020-05-09 18:17:35 +0800

    SQL

--- Database/SQL/SQL经典实例/MySQL/高级查询.sql
+++ Database/SQL/SQL经典实例/MySQL/高级查询.sql
@@ -3,24 +3,27 @@
   LIMIT 子句指定要返回的行数
   OFFSET 子句指定要跳过的行数
  */
-select sal
-from emp
-order by sal limit 5 offset 0;
+SELECT sal
+FROM emp
+ORDER BY sal LIMIT 5 OFFSET 0;
 
 
 -- 2、跳过n行记录
 -- where b.ename <= a.ename 模拟排序，ename按照姓名来排序（字典顺序）
-select x.ename
-from (select a.ename,
-             (select count(*) from emp b where b.ename <= a.ename) as rn
-      from emp a)x
-where mod(x.rn, 2) = 1;
+SELECT x.ename
+FROM (SELECT a.ename,
+             (SELECT COUNT(*) FROM emp b WHERE b.ename <= a.ename) AS rn
+      FROM emp a)X
+WHERE MOD(x.rn, 2) = 1;
 
 -- 3、在外连接查询里使用OR逻辑
-select e.ename, d.deptno, d.dname, d.loc
-from dept d
-       left join emp e on (d.deptno = e.deptno
-                             and (e.deptno = 10 or e.deptno = 20))
-order by 2;
+SELECT e.ename, d.deptno, d.dname, d.loc
+FROM dept d
+       LEFT JOIN emp e ON (d.deptno = e.deptno
+                             AND (e.deptno = 10 OR e.deptno = 20))
+ORDER BY 2;
 
--- 4、识别互逆的记录
+-- 4、提取最靠前的N行记录
+
+
+
